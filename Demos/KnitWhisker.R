695a015c0e65d292eb5174bb4dbf5e89073e0b1b
BFannin <BFannin@RedwoodsGroup.com>
2015-09-30 10:24:16 -0400

    Initial commit of demo files

---
+++ Demos/KnitWhisker.R
@@ -0,0 +1,9 @@
+library(whisker)
+data <- list(Hitter1 = "Sammy Sosa"
+             , Hitter2 = "Pete Rose"
+             , NameId1 = "sosasa01"
+             , NameId2 = "rosepe01")
+
+outFile <- whisker.render(readLines("Example_Whisker.Rmd"), data)
+writeLines(outFile, "SosaVsRose.Rmd")
+rmarkdown::render("SosaVsRose.Rmd", envir = new.env(), runtime = "static", quiet = TRUE)
