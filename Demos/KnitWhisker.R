20d4b4f64d8035a5738991498ea2d519ff3f6c10
BFannin <BFannin@RedwoodsGroup.com>
2015-10-05 09:37:28 -0400

    Minor changes from live talk

--- Demos/KnitWhisker.R
+++ Demos/KnitWhisker.R
@@ -4,6 +4,6 @@ data <- list(Hitter1 = "Sammy Sosa"
              , NameId1 = "sosasa01"
              , NameId2 = "rosepe01")
 
-outFile <- whisker.render(readLines("Example_Whisker.Rmd"), data)
+outFile <- whisker.render(readLines("Demos/Example_Whisker.Rmd"), data)
 writeLines(outFile, "SosaVsRose.Rmd")
 rmarkdown::render("SosaVsRose.Rmd", envir = new.env(), runtime = "static", quiet = TRUE)
